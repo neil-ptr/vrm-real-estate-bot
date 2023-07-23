d23e766006653de4569284b49ae13c3f2c189992
Derryk Boyd <derryk@redmantech.com>
2023-07-23 16:44:17 -0600

    Fix hydration errors

--- src/components/MusicToggle.tsx
+++ src/components/MusicToggle.tsx
@@ -1,40 +1,30 @@
 "use client";
 import useSound from "use-sound";
 import { Switch } from "./ui/switch";
-import { useEffect } from "react";
-import { useLocalStorage } from "usehooks-ts";
+import { useEffect, useState } from "react";
+import { cn } from "~/utils/utils";
 
 export function MusicToggle() {
-  const [musicEnabled, setMusicEnabled] = useLocalStorage("musicEnabled", true);
   const [play, { stop }] = useSound("/sounds/bg-music.mp4", { volume: 0.5 });
 
   useEffect(() => {
-    if (musicEnabled) {
-      play();
-    }
-
     return () => {
       stop();
     };
-  }, [play, stop, musicEnabled]);
+  }, [stop]);
 
   return (
-    <div className="flex gap-4">
+    <div className={cn(["flex gap-4"])}>
       <Switch
-        checked={musicEnabled}
         onCheckedChange={(checked) => {
           if (checked) {
             play();
-            localStorage.setItem("musicEnabled", "true");
-            setMusicEnabled(true);
           } else {
             stop();
-            localStorage.setItem("musicEnabled", "false");
-            setMusicEnabled(false);
           }
         }}
       />
-      <p>Turn Music {musicEnabled ? "Off" : "On"}</p>
+      <p>Enable Music</p>
     </div>
   );
 }
